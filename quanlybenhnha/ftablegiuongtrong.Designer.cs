026279accbfcdcb8f1a5b765506c03ebb67b3633
tramy984 <145307628+tramy984@users.noreply.github.com>
2023-09-18 20:55:17 +0700

    Add files via upload

---
+++ quanlybenhnha/ftablegiuongtrong.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace quanlybenhnha
+{
+    partial class ftablegiuongtrong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            button12 = new Button();
+            button11 = new Button();
+            button10 = new Button();
+            button9 = new Button();
+            button8 = new Button();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            label1 = new Label();
+            panel2 = new Panel();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label7 = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button12);
+            panel1.Controls.Add(button11);
+            panel1.Controls.Add(button10);
+            panel1.Controls.Add(button9);
+            panel1.Controls.Add(button8);
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(12, 21);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(772, 829);
+            panel1.TabIndex = 0;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(529, 610);
+            button12.Name = "button12";
+            button12.Size = new Size(221, 125);
+            button12.TabIndex = 12;
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(280, 610);
+            button11.Name = "button11";
+            button11.Size = new Size(221, 125);
+            button11.TabIndex = 11;
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(30, 610);
+            button10.Name = "button10";
+            button10.Size = new Size(221, 125);
+            button10.TabIndex = 10;
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(529, 445);
+            button9.Name = "button9";
+            button9.Size = new Size(221, 125);
+            button9.TabIndex = 9;
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(280, 445);
+            button8.Name = "button8";
+            button8.Size = new Size(221, 125);
+            button8.TabIndex = 8;
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(30, 445);
+            button7.Name = "button7";
+            button7.Size = new Size(221, 125);
+            button7.TabIndex = 7;
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(529, 275);
+            button6.Name = "button6";
+            button6.Size = new Size(221, 125);
+            button6.TabIndex = 6;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(280, 275);
+            button5.Name = "button5";
+            button5.Size = new Size(221, 125);
+            button5.TabIndex = 5;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(30, 275);
+            button4.Name = "button4";
+            button4.Size = new Size(221, 125);
+            button4.TabIndex = 4;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(529, 110);
+            button3.Name = "button3";
+            button3.Size = new Size(221, 125);
+            button3.TabIndex = 3;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(280, 110);
+            button2.Name = "button2";
+            button2.Size = new Size(221, 125);
+            button2.TabIndex = 2;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(30, 110);
+            button1.Name = "button1";
+            button1.Size = new Size(221, 125);
+            button1.TabIndex = 1;
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(166, 25);
+            label1.Name = "label1";
+            label1.Size = new Size(384, 37);
+            label1.TabIndex = 0;
+            label1.Text = "Danh sách giường bệnh";
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(label7);
+            panel2.Controls.Add(textBox5);
+            panel2.Controls.Add(label2);
+            panel2.Controls.Add(textBox4);
+            panel2.Controls.Add(label3);
+            panel2.Controls.Add(textBox3);
+            panel2.Controls.Add(textBox1);
+            panel2.Controls.Add(label6);
+            panel2.Controls.Add(textBox2);
+            panel2.Controls.Add(label5);
+            panel2.Controls.Add(label4);
+            panel2.Location = new Point(790, 21);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(718, 829);
+            panel2.TabIndex = 1;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(348, 562);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(348, 39);
+            textBox5.TabIndex = 20;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(348, 465);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(348, 39);
+            textBox4.TabIndex = 19;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(348, 364);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(348, 39);
+            textBox3.TabIndex = 18;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.Location = new Point(3, 564);
+            label6.Name = "label6";
+            label6.Size = new Size(302, 37);
+            label6.TabIndex = 17;
+            label6.Text = "Tên y tá phụ trách:";
+            label6.Click += label6_Click;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.Location = new Point(3, 467);
+            label5.Name = "label5";
+            label5.Size = new Size(339, 37);
+            label5.TabIndex = 16;
+            label5.Text = "Tên bác sĩ phụ trách:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.Location = new Point(3, 363);
+            label4.Name = "label4";
+            label4.Size = new Size(252, 37);
+            label4.TabIndex = 15;
+            label4.Text = "Tên bệnh nhân:";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(348, 272);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(348, 39);
+            textBox2.TabIndex = 14;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(348, 188);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(348, 39);
+            textBox1.TabIndex = 13;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.Location = new Point(3, 272);
+            label3.Name = "label3";
+            label3.Size = new Size(190, 37);
+            label3.TabIndex = 12;
+            label3.Text = "Trạng thái :";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label2.Location = new Point(3, 187);
+            label2.Name = "label2";
+            label2.Size = new Size(264, 37);
+            label2.TabIndex = 11;
+            label2.Text = "ID giường bệnh:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Arial", 12F, FontStyle.Bold, GraphicsUnit.Point);
+            label7.Location = new Point(189, 25);
+            label7.Name = "label7";
+            label7.Size = new Size(378, 37);
+            label7.TabIndex = 21;
+            label7.Text = "Thông tin giường bệnh:";
+            // 
+            // ftablegiuongtrong
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1532, 879);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "ftablegiuongtrong";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Button button1;
+        private Label label1;
+        private Button button12;
+        private Button button11;
+        private Button button10;
+        private Button button9;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Panel panel2;
+        private TextBox textBox5;
+        private Label label2;
+        private TextBox textBox4;
+        private Label label3;
+        private TextBox textBox3;
+        private TextBox textBox1;
+        private Label label6;
+        private TextBox textBox2;
+        private Label label5;
+        private Label label4;
+        private Label label7;
+    }
+}
